529f0027f8a84e6ea1f0a34bb078422400c70f11
Abhikalp Srivastava <abhikalp15092003@gmail.com>
2025-06-23 22:44:01 +0530

    Update educationData.ts

--- src/data/educationData.ts
+++ src/data/educationData.ts
@@ -8,22 +8,22 @@ interface Education {
 
 export const educationData: Education[] = [
   {
-    institution: 'International Institute of Information Technology Bhubaneswar',
-    degree: 'Bachelor of Technology in Computer Science and Engineering',
+    institution: 'International Institute of Information Technology Bhubaneswar (IIIT Bhubaneswar)',
+    degree: 'Bachelor of Technology (B.Tech) - Computer Science and Engineering',
     date: 'June 2025',
     location: 'Bhubaneswar, India',
-    gpa: '9.25/10.0'
+    gpa: '9.27/10.0'
   },
   {
     institution: 'Royale Concorde International School',
-    degree: '12th std CBSE',
+    degree: '12th Standard - CBSE',
     date: 'July 2021',
     location: 'Bangalore, India',
     gpa: '96.4%'
   },
   {
     institution: 'ITI Central School',
-    degree: '10th std CBSE',
+    degree: '10th Standard - CBSE',
     date: 'May 2019',
     location: 'Bangalore, India',
     gpa: '96.8%'
